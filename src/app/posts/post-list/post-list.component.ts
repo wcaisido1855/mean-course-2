a6c4a3f46b7b08a01396ae355121b90aef35daaf
WilliamCaisido <wcaisido1855@gmail.com>
2019-03-03 10:46:54 -0800

    Commit 1.4
    Section 2 - Lesson 22. Timestamp 04:36 / 5:40

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -2,6 +2,15 @@ import { Component } from '@angular/core';
 
 @ Component({
   selector: 'app-post-list',
-  templateUrl: './post-list.component.html'
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {}
+export class PostListComponent {
+//  posts = [
+//    {title: 'First Post', content: 'This is the first post\'s content'},
+//    {title: 'Second Post', content: 'This is the second post\'s content'},
+//    {title: 'Third Post', content: 'This is the third post\'s content'},
+//  ];
+
+  posts = [];
+}
