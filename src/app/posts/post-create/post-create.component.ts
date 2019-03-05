a3fa0d643d3d8a665d53a632a6cee3397e8f5d42
WilliamCaisido <wcaisido1855@gmail.com>
2019-03-05 13:34:56 -0800

    Commit 1.8
    Completed Section 2, Lecture 25

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -14,6 +14,9 @@ export class PostCreateComponent {
   @ Output() postCreated = new EventEmitter< Post>();
 
   onAddPost(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
     const post: Post = {
       title: form.value.title,
       content: form.value.content
