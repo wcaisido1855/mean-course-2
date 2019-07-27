b1598774682afc5a24299ca49c2a57ca408b1b3a
wcaisido1855 <wcaisido1855@gmail.com>
2019-07-27 08:49:26 -0700

    Commit 1.28
    Added Image picker functionality to Post-Create. Completed through lecture 74. Now on to 75.
    
    Also, remember to add comments in code using /* */ so that you can go bakc and actually read /understand  what you've created

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,21 +1,21 @@
-import { Component, OnInit } from "@angular/core";
-import { NgForm } from "@angular/forms";
-import { ActivatedRoute, ParamMap } from "@angular/router";
-
-import { PostsService } from "../posts.service";
-import { Post } from "../post.model";
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { PostsService } from '../posts.service';
+import { Post } from '../post.model';
 
 @Component({
-  selector: "app-post-create",
-  templateUrl: "./post-create.component.html",
-  styleUrls: ["./post-create.component.css"]
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
 })
 export class PostCreateComponent implements OnInit {
-  enteredTitle = "";
-  enteredContent = "";
+  enteredTitle = '';
+  enteredContent = '';
   post: Post;
   isLoading = false;
-  private mode = "create";
+  form: FormGroup;
+  private mode = 'create';
   private postId: string;
 
   constructor(
@@ -23,11 +23,23 @@ export class PostCreateComponent implements OnInit {
     public route: ActivatedRoute
   ) {}
 
+  /*Initialize The FormControl Variables - AKA the inputs, so that the exist, and I can define the lopgic for how they work later*/
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)]
+      }),
+      content: new FormControl(null, {
+        validators: [Validators.required]
+      }),
+      image: new FormControl(null, {
+        validators: [Validators.required]
+      })
+    });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
-      if (paramMap.has("postId")) {
-        this.mode = "edit";
-        this.postId = paramMap.get("postId");
+      if (paramMap.has('postId')) {
+        this.mode = 'edit';
+        this.postId = paramMap.get('postId');
         this.isLoading = true;
         this.postsService.getPost(this.postId).subscribe((postData) => {
           this.isLoading = false;
@@ -36,28 +48,41 @@ export class PostCreateComponent implements OnInit {
             title: postData.title,
             content: postData.content
           };
+          this.form.setValue({
+            title: this.post.title,
+            content: this.post.content
+          });
         });
       } else {
-        this.mode = "create";
+        this.mode = 'create';
         this.postId = null;
       }
     });
   }
 
-  onSavePost(form: NgForm) {
-    if (form.invalid) {
+  /*Logic for the Image picker*/
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
+    console.log(file);
+    console.log(this.form);
+  }
+
+  onSavePost() {
+    if (this.form.invalid) {
       return;
     }
     this.isLoading = true;
-    if (this.mode === "create") {
-      this.postsService.addPost(form.value.title, form.value.content);
+    if (this.mode === 'create') {
+      this.postsService.addPost(this.form.value.title, this.form.value.content);
     } else {
       this.postsService.updatePost(
         this.postId,
-        form.value.title,
-        form.value.content
+        this.form.value.title,
+        this.form.value.content
       );
     }
-    form.resetForm();
+    this.form.reset();
   }
 }
