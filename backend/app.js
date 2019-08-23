71b41e371a1ef2d754f364acd04c22b7d721717e
wcaisido1855 <wcaisido1855@gmail.com>
2019-08-23 13:59:12 -0700

    Commit 1.34
    finished Lecture # 82 - Images showing onposts and previews correctly. On to #83

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const postsRoutes = require("./routes/posts");
-const Post = require("./models/post");
+const postsRoutes = require('./routes/posts');
+const Post = require('./models/post');
 
 const app = express();
 
@@ -11,30 +12,35 @@ const app = express();
 // Mongo PW: Password1
 // Mongo Connection String: mongodb+srv://Will_Brogrammer:<password>@mean-cluster-swfhi.mongodb.net/test?retryWrites=true&w=majority
 
-mongoose.connect("mongodb+srv://Will_Brogrammer:Password1@mean-cluster-swfhi.mongodb.net/node-angular?retryWrites=true&w=majority", { useNewUrlParser: true })
-.then(() => {
-  console.log("Connected to database!");
-})
-.catch(() => {
-  console.log("Connection failed!");
-});
+mongoose
+  .connect(
+    'mongodb+srv://Will_Brogrammer:Password1@mean-cluster-swfhi.mongodb.net/node-angular?retryWrites=true&w=majority',
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log('Connected to database!');
+  })
+  .catch(() => {
+    console.log('Connection failed!');
+  });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use('/images', express.static(path.join('backend/images')));
 
 app.use((req, res, next) => {
-res.setHeader("Access-Control-Allow-Origin", "*");
-res.setHeader(
-  "Access-Control-Allow-Headers",
-  "Origin, X-Requested-With, Content-Type, Accept"
-);
-res.setHeader(
-  "Access-Control-Allow-Methods",
-  "GET, POST, PATCH, PUT, DELETE, OPTIONS"
-);
-next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+  next();
 });
 
-app.use("/api/posts", postsRoutes);
+app.use('/api/posts', postsRoutes);
 
 module.exports = app;
